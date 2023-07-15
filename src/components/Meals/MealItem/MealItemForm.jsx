c95829bc268fa6f8c8e0c1aab6bed07906926e8d
Bruno Alpachino <brunolalpachino@gmail.com>
2023-07-15 07:21:41 +0300

    created an input component

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 const MealItemForm = (props) => {
   return (
     <div>
       <form>
-        <label></label>
-        <input></input>
+        <Input
+          label="Amount"
+          input={{
+            id: "amount",
+            type: "number",
+            min: "1",
+            max: "5",
+            step: "1",
+            defaultValue: "1",
+          }}
+        />
         <bUtton>add to cart</bUtton>
       </form>
     </div>
