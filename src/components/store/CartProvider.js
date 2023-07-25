a374e56a4439058f4326fd63aff26bd1f8434dcb
Bruno Alpachino <brunolalpachino@gmail.com>
2023-07-26 00:51:14 +0300

    passed down the cart value to the cart

--- src/components/store/CartProvider.js
+++ src/components/store/CartProvider.js
@@ -1,6 +1,21 @@
 import cartContext from "./cart-context";
 const cartProvider = (props) => {
-  return <cartContext.Provider>{props.children}</cartContext.Provider>;
+  const addItemToCart = (item) => {};
+  const removeItemFromCart = (id) => {};
+
+  //   this will be passed down as the value that is suppossed to be accessed
+  // by other components that need the properties that are in context
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCart,
+    removeItem: removeItemFromCart,
+  };
+  return (
+    <cartContext.Provider value={cartContext}>
+      {props.children}
+    </cartContext.Provider>
+  );
 };
 
 export default cartProvider;
